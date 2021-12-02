d5d350e27250bfffc217441e136492f707785162
Yamin Nather <2001s.yn@gmail.com>
2021-12-02 06:58:01 +0530

    Changed database name to the new one from the old one before switching from local mongodb to mongodb atlas

--- src/mongodb.ts
+++ src/mongodb.ts
@@ -1,10 +1,14 @@
 import { Db, MongoClient } from "mongodb";
 
-const mongoClient: MongoClient = new MongoClient("mongodb://localhost:27017/?directConnection=true&serverSelectionTimeoutMS=2000");
+// const mongoClient: MongoClient = new MongoClient("mongodb://localhost:27017/?directConnection=true&serverSelectionTimeoutMS=2000");
+const uri = "mongodb+srv://Admin:%23GomaalaPunda06@cluster0.rbh2g.mongodb.net/database?retryWrites=true&w=majority";
+const mongoClient = new MongoClient(uri);
 
 export async function connectToMongo(): Promise<void> {
   await mongoClient.connect();
 }
 
-const database: Db = mongoClient.db("test");
+// const database: Db = mongoClient.db("test");
+const database: Db = mongoClient.db("database");
+
 export default database;
